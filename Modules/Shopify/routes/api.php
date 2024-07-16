c9e1a4587f594346fc188b4ebc9504e5347c1f79
kailash-hitechvalley <sanjogkarki1460>
2024-07-16 14:39:17 +0545

    sss

--- Modules/Shopify/routes/api.php
+++ Modules/Shopify/routes/api.php
@@ -1,69 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Shopify\App\Http\Controllers\Middleware\Erply\CollectionController;
-use Modules\Shopify\App\Http\Controllers\Middleware\Erply\ImagesController;
-use Modules\Shopify\App\Http\Controllers\Middleware\Erply\LocationController;
-use Modules\Shopify\App\Http\Controllers\Middleware\Erply\PriceController;
-use Modules\Shopify\App\Http\Controllers\Middleware\Erply\ProductController;
-use Modules\Shopify\App\Http\Controllers\Middleware\Erply\SohController;
-use Modules\Shopify\App\Http\Controllers\ReadShopify\OrderController;
-use Modules\Shopify\App\Http\Controllers\ReadShopify\ShopifyController;
-use Modules\Shopify\App\Http\Controllers\ReadShopify\ShopifyProductsController;
-use Modules\Shopify\App\Http\Controllers\ReadShopify\ShopifyWebHookController;
-use Modules\Shopify\App\Http\Controllers\ViewController\OrderViewController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\GiftCardController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\ShopifyCustomerController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\SourceProductController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\SourceCollectionController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\SourceImageController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\SourcePriceController;
-use Modules\Shopify\App\Http\Controllers\WriteShopify\SourceSohController;
+use Modules\Shopify\Http\Controllers\ShopifyController;
 
+/*
+ *--------------------------------------------------------------------------
+ * API Routes
+ *--------------------------------------------------------------------------
+ *
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * is assigned the "api" middleware group. Enjoy building your API!
+ *
+*/
 
-Route::get('refunds', [ShopifyController::class, 'getRefund']);
-Route::get('getorders', [OrderController::class, 'getOrders']);
-Route::get('getrefunds', [OrderController::class, 'getRefunds']);
-
-# routes for shopify  push
-Route::get('push-products', [SourceProductController::class, 'index']);
-Route::get('push-collections', [SourceCollectionController::class, 'index']);
-Route::get('push-images', [SourceImageController::class, 'index']);
-Route::get('add-variants-media', [SourceImageController::class, 'addvariantsToMedia']);
-Route::get('push-product-soh', [SourceSohController::class, 'index']);
-Route::get('push-price', [SourcePriceController::class, 'index']);
-
-# move products details from erplay to module
-
-Route::get('get-products', [ProductController::class, 'getProducts']);
-
-Route::get('get-category', [CollectionController::class, 'getCategory']);
-
-Route::get('get-locations', [LocationController::class, 'getLocation']);
-
-Route::get('get-images', [ImagesController::class, 'index']);
-
-Route::get('get-soh', [SohController::class, 'index']);
-
-Route::get('get-price', [PriceController::class, 'index']);
-#Web Hooks
-
-Route::post('shopify/webhooks/orders', [ShopifyWebHookController::class, 'handleOrderwebHooks']);
-
-Route::get('get-tags', [ProductController::class, 'getTags']);
-
-
-#Shopify Products
-
-Route::get('getShopifyProducts', [ShopifyProductsController::class, 'getProducts']);
-Route::get('getVariantsProducts', [ShopifyProductsController::class, 'getVariantsProducts']);
-
-Route::get('delete-laravel-logs', [OrderViewController::class, 'deleteLaravelLogs']);
-#Route::post('webhook-product', [ShopifyProductsController::class, 'handleProductwebHooks']);
-Route::post('webhook-product', [ShopifyProductsController::class, 'handleProductwebHooks']);
-
-#get customers from the Shopify
-Route::get('/get-shopify-customers', [ShopifyCustomerController::class, 'index']);
-
-#push the Gift cards to the Shopify
-Route::get('/push-gift-card', [GiftCardController::class, 'index']);
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('shopify', ShopifyController::class)->names('shopify');
+});

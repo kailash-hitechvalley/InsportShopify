d4cef9b4b3af0b2380fdc56669dd7fbf95173f0e
kailash-hitechvalley <sanjogkarki1460>
2024-07-29 14:14:44 +0545

    get soh

--- Modules/Shopify/app/Http/Controllers/WriteShopify/SourceSohController.php
+++ Modules/Shopify/app/Http/Controllers/WriteShopify/SourceSohController.php
@@ -289,7 +289,7 @@ class SourceSohController extends Controller
             }
         }
         if (count(array_unique($parent)) > 0) {
-
+            echo "multiple parent variants found=>" . implode(',', array_unique($parent));
             return  $this->productService->updateProduct($productid, [
                 'sohPendingProcess' => 10,
                 'lastPushedDate' => date('Y-m-d H:i:s'),

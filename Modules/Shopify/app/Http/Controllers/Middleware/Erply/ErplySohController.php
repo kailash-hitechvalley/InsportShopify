754831bc1919f0a4caae8e43054798058928399b
kailash-hitechvalley <sanjogkarki1460>
2024-08-01 09:22:24 +0545

    MultipleParentVariantFound

--- Modules/Shopify/app/Http/Controllers/Middleware/Erply/ErplySohController.php
+++ Modules/Shopify/app/Http/Controllers/Middleware/Erply/ErplySohController.php
@@ -85,7 +85,7 @@ class ErplySohController extends Controller
             $variationSohs = $this->productService->getVariantSoh($Variant->productID);
 
             if (@$variationSohs) {
-
+                echo "soh found =>" . count($variationSohs);
                 $res =  $this->manageSoh($variationSohs, $Variant);
 
                 if ($res == 4) {

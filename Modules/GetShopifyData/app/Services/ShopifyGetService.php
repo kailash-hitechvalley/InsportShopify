69c1ea1283d6e6694edc6bb91030ebd92261c75b
kailash-hitechvalley <sanjogkarki1460>
2024-08-28 15:14:00 +0545

    get only products

--- Modules/GetShopifyData/app/Services/ShopifyGetService.php
+++ Modules/GetShopifyData/app/Services/ShopifyGetService.php
@@ -173,7 +173,7 @@ class ShopifyGetService
             $after = $cursor ? ', after: "' . $cursor . '"' : '';
         } else {
             $magic = "'" . $cursor . "'";
-            $after = ' ,sortKey:UPDATED_AT, query: "updated_at:>=' . $magic  . ' "';
+            $after = ', query: "updated_at:>=' . $magic  . ' "';
         }
 
         $query = '{
